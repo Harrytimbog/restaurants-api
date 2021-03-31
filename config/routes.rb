946d241b35a50e149c049fc2cbacf2be1ee196e6
Timilehin Emmanuel <Harriemannie@gmail.com>
2021-03-31 13:06:59 +0100

    updated Ruby gems

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :restaurants, only: [ :index, :show ]
+      resources :restaurants, only: [ :index, :show, :update, :create, :destroy ]
     end
   end  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
 end
